727a9b4f08245771980c662b681f06ea7854afd6
phrush <w.phrush@gmail.com>
2017-04-21 16:04:01 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const height = window.innerHeight;
+
+const scrollOnePage = () => {
+    console.log(height);
+    window.scrollTo(0, height);
+}
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '/')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(3000, function() {
+  console.log('listening at http://localhost:3000');
+});
